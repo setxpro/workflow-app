de194266ccb55384e4209a76511d1d7fd8dc8036
Patrick Anjos <69186374+setxpro@users.noreply.github.com>
2023-04-03 13:04:13 -0300

    alter router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth";
 import GetThemeProvider from "./contexts/Theme/GetThemeContext";
 
